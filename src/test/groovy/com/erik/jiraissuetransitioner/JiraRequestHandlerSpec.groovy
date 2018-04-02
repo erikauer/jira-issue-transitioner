43d310279ac3c28afedc094d959b77e7b7f22270
erikauer <erikauer@gmx.at>
2018-04-02 21:52:07 +0200

    refactor(JiraRequestHandlerSpec.groovy): Rename file to more common name convention

--- src/test/groovy/com/erik/jiraissuetransitioner/JiraRequestHandlerSpec.groovy
+++ src/test/groovy/com/erik/jiraissuetransitioner/JiraRequestHandlerSpec.groovy
@@ -2,7 +2,7 @@ package com.erik.jiraissuetransitioner
 
 import spock.lang.Specification
 
-class JiraRequestHandlerTest extends Specification {
+class JiraRequestHandlerSpec extends Specification {
     def "length of Spock's and his friends' names"() {
         expect:
         name.size() == length
